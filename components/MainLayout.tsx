be15863941b16ffcb6cae45a685b826bceca5e12
zenkun1234411 <zenkun1234411@gmail.com>
2026-02-25 18:08:11 +0800

    Added conditional layout

--- components/MainLayout.tsx
+++ components/MainLayout.tsx
@@ -1,16 +1,10 @@
 import Sidebar from '@/components/Sidebar';
 
-export default function MainLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
+export default function MainLayout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="flex">
-      <Sidebar/>
-      <main className="flex-1 p-6 bg-gray-100 min-h-screen">
-        {children}
-      </main>
+    <div className="min-h-screen flex">
+      <Sidebar />
+      <main className="flex-1 p-6 bg-gray-100">{children}</main>
     </div>
   );
 }
