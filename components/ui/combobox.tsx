6b195934968d3c4fcfebb680acce549adb1adb6e
zenkun1234411 <zenkun1234411@gmail.com>
2026-03-04 03:44:07 +0800

    fixed merge conflict

--- components/ui/combobox.tsx
+++ components/ui/combobox.tsx
@@ -148,11 +148,7 @@ function ComboboxItem({
     <ComboboxPrimitive.Item
       data-slot="combobox-item"
       className={cn(
-<<<<<<< HEAD
-        "relative flex w-full cursor-default items-center gap-2 rounded-sm py-1.5 pr-8 pl-2 text-sm outline-hidden select-none data-highlighted:bg-accent data-highlighted:text-accent-foreground data-disabled:pointer-events-none data-disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:shrink-0 [&_svg:not([class*='size-'])]:size-4",
-=======
         "relative flex w-full cursor-default items-center gap-2 rounded-sm py-1.5 pr-8 pl-2 text-sm outline-hidden select-none data-highlighted:bg-accent data-highlighted:text-accent-foreground data-[disabled]:pointer-events-none data-[disabled]:opacity-50 [&_svg]:pointer-events-none [&_svg]:shrink-0 [&_svg:not([class*='size-'])]:size-4",
->>>>>>> 07a1f80810e1e9f145b645561b33bc7dae2dad0d
         className
       )}
       {...props}
