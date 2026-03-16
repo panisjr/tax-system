140fd87e6e26b169747ce176f84e6456ccc1d029
DevShean <tanamor.shn@gmail.com>
2026-03-16 14:56:25 +0800

    splash loading screen done

---
+++ app/loading.tsx
@@ -0,0 +1,5 @@
+import AppSplashScreen from "@/components/AppSplashScreen";
+
+export default function Loading() {
+  return <AppSplashScreen />;
+}
---
+++ components/InitialSplashGate.tsx
@@ -0,0 +1,31 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import AppSplashScreen from "@/components/AppSplashScreen";
+
+type InitialSplashGateProps = {
+  children: React.ReactNode;
+};
+
+const SPLASH_DURATION_MS = 5000;
+
+export default function InitialSplashGate({ children }: InitialSplashGateProps) {
+  const [showSplash, setShowSplash] = useState(true);
+
+  useEffect(() => {
+    const timeoutId = window.setTimeout(() => {
+      setShowSplash(false);
+    }, SPLASH_DURATION_MS);
+
+    return () => {
+      window.clearTimeout(timeoutId);
+    };
+  }, []);
+
+  return (
+    <>
+      {children}
+      {showSplash && <AppSplashScreen />}
+    </>
+  );
+}
